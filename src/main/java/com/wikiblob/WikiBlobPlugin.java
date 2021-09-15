84dbd96dcba8ee1adc2084a514769b3db3a55203
Ewlame <elaineholdsworth@hotmail.com>
2021-09-15 23:41:22 +1000

    Setup

--- src/main/java/com/wikiblob/WikiBlobPlugin.java
+++ src/main/java/com/wikiblob/WikiBlobPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.wikiblob;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Wiki Blob"
 )
-public class ExamplePlugin extends Plugin
+public class WikiBlobPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private WikiBlobConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	WikiBlobConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(WikiBlobConfig.class);
 	}
 }
